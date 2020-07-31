8249e8a2ceab121cbc5d039a1bfdeb5c7adf5174
Lokesh Kumar <kumarlokesh097@gmail.com>
2020-07-31 18:14:12 +0530

    minor changes

--- MgSwipeTableViewSwift/Sources/MGSwipeButtonsView.swift
+++ MgSwipeTableViewSwift/Sources/MGSwipeButtonsView.swift
@@ -7,3 +7,26 @@
 //
 
 import Foundation
+import UIKit
+
+class MGSwipeButtonsView: UIView {
+    weak var cell: MGSwipeTableCell?
+    var backgroundColorCopy: UIColor?
+    
+    var _buttons: [AnyHashable]? = nil
+    var _container: UIView? = nil
+    var _fromLeft: Bool? = false
+    var _expandedButton: UIView? = nil
+    var _expandedButtonAnimated: UIView? = nil
+    var _expansionBackground: UIView? = nil
+    var _expansionBackgroundAnimated: UIView? = nil
+    var _expandedButtonBoundsCopy: CGRect = CGRect.init()
+    var _direction: MGSwipeDirection?
+    var _expansionLayout: MGSwipeExpansionLayout?
+    var _expansionOffset: CGFloat = 0.0
+    var _buttonsDistance: CGFloat = 0.0
+    var _safeInset: CGFloat = 0.0
+    var _autoHideExpansion: Bool = false
+}
+
+
