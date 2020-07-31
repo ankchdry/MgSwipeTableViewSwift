ac41387ea7f29ddd3f53b19329343ad6289078ce
Ankit Chaudhary <ankit2.chaudhary@paytm.com>
2020-07-31 15:46:42 +0530

    Initial Commit

---
+++ MgSwipeTableViewSwift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MgSwipeTableViewSwift
+//
+//  Created by Ankit Chaudhary on 31/07/20.
+//  Copyright © 2020 Ankit Chaudhary. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
