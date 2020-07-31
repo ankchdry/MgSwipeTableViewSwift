aea15420894f710533e7a3d68e4c42a8ee5cfdfc
Ankit Chaudhary <ankit2.chaudhary@paytm.com>
2020-07-31 15:56:11 +0530

    Added MGSwipeTableViewCell Files

---
+++ MgSwipeTableViewSwift/Sources/MGSwipeAnimationData.swift
@@ -0,0 +1,9 @@
+//
+//  MGSwipeAnimationData.swift
+//  MgSwipeTableViewSwift
+//
+//  Created by Ankit Chaudhary on 31/07/20.
+//  Copyright © 2020 Ankit Chaudhary. All rights reserved.
+//
+
+import Foundation
